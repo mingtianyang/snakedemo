9faab51e22e4b83ec8a23e0484428c20bab15cc6
ming <ming@qq.com>
2017-06-01 19:54:47 +0800

    version2.0.1 testone

--- js/Snake.js
+++ js/Snake.js
@@ -1,239 +1,123 @@
-const northImg=new Image();
-northImg.src="image/north.png";//上
-const southImg=new Image();
-southImg.src="image/south.png";//下
-const eastImg=new Image();
-eastImg.src="image/east.png";//右
-const westImg=new Image();
-westImg.src="image/west.png";//左
-const bodyImg=new Image();
-bodyImg.src="image/body.png";//体
-const foodImg=new Image();
-foodImg.src="image/food.png";//食
-const startImg=new Image();
-startImg.src="image/start.png";//将欢迎页面放在最后
-const bgImg=new Image();
-bgImg.src="image/background.png";//开始页面
-function Snake(){
-	this.canvas=$("#gameview")[0]//canvas画布对象；
-	this.ctx=this.canvas.getContext("2d");//画笔
-	this.width=500;//背景(游戏屏幕)宽度
-	this.height=500;//背景高度
-	this.step=25;//设计步长
-	this.stepX=Math.floor(this.width/this.step);//X轴的步数
-	this.stepY=Math.floor(this.height/this.step);//Y轴的步数
-	this.snakeBodyList=[];//蛇身数组
-	this.foodList=[];//食物数组
-	this.isDead=false;//蛇是否活着标示位
-    this.isEaten=false;//蛇是否碰到自身
-    this.score = 0;//分数  +10  存入到localStorage中
-    this.isPhone=false;
-	/*
-	 1.生成初始化页面点击页面进入游戏
-	 * */
+//ES6 全局变量使用const关键字定义：  只读
+const northImg = new Image();
+northImg.src = "image/north.png";
+const southImg = new Image();
+southImg.src = "image/south.png";
+const eastImg = new Image();
+eastImg.src = "image/east.png";
+const westImg = new Image();
+westImg.src = "image/west.png";
+const bodyImg = new Image();
+bodyImg.src = "image/body.png";
+const foodImg = new Image();
+foodImg.src = "image/food1.png";
+var bgImg = new Image();
+bgImg.src = "image/background.png";
+//将欢迎界面的图片放在最后，表示加载成功后，其他图片已经加载完毕，无需再进行onload判断
+const startImg = new Image();
+startImg.src = "image/start.png";
+
+//点击改变背景图片的函数(事件委托)
+$("#myul").on("click","li",function(){
+	bgImg.src="image/bg"+($(this).index()+1)+".png";
+})
+	
+function Snake() {
+
+	this.canvas = $("#gameview")[0]; //canvas画布对象
+	this.ctx = this.canvas.getContext("2d"); //画笔
+	this.width = 500; //背景（游戏屏幕）的宽度
+	this.height = 500; //背景（游戏屏幕）的高度
+	this.step = 25; //设计步长
+	this.stepX = Math.floor(this.width / this.step); //X轴步数
+	this.stepY = Math.floor(this.height / this.step); //Y轴步数
+	this.snakeBodyList = []; //设置蛇身数组
+	this.foodList = []; //设置食物数组
+	this.timer = null;//蛇动时的定时器
+	this.score = 0;//分数  +10  存入到localStorage中
+	this.isDead = false;//蛇是否活着标识位
+	this.isEaten = false;//食物是否被吃掉标识位
+	this.isPhone = false;//判断设备是否为移动端  true--移动端  false--PC
 
 	/*
-	 2.游戏开始，绘制背景、蛇、食物
-	 * */
-	this.open=function(){
-		this.device();
-		this.start();
-		//2.4蛇移动
-		this.score=0;//积分清零
+	 * 1-生成初始化页面，点击该页面，进入游戏
+	 */
+	
+	/*
+	 * 2-游戏开始，绘制背景、蛇、食物,蛇移动
+	 */
+	
+	this.start = function(){
+		this.device();//判断设备类型
+		this.score = 0;//积分清零
+		this.paint();
 		
-		this.move();
 	}
-	this.start=function(){
+	
+	/*
+	 * 判断当前设备是否是移动端
+	 */
+	this.device = function(){
+		//1-读取BOM对象navigator的userAgent信息
+		var deviceInfo = navigator.userAgent;
+		//2-判断是否为PC端（是否含有Windows字符串）
+		if(deviceInfo.indexOf("Windows") == -1){
+			this.isPhone = true;
+			this.canvas.width = 414;
+			this.canvas.height = 583;
+			this.width = 414;
+			this.height = 583;
+			this.stepX = this.width/this.step;
+			this.stepY = this.height/this.step;
+//			console.log(this.width+":"+this.height);
+		}
 		
-		//2.1画出背景
-		this.ctx.drawImage(bgImg,0,0,this.width,this.height);
-		//2.2画蛇
+	}
+	
+	/*
+	 * 绘制背景、蛇、食物
+	 */
+	this.paint = function() {
+		//2.1 画出背景
+		this.ctx.drawImage(bgImg, 0, 0,this.width,this.height);
+		//2.2 画蛇
 		this.drawSnake();
-		//2.3画食物
+		//2.3 随机画出食物
 		this.drawFood();
-		
-	}
-	/* 
-	 *判断当前设备是否为移动端*/
-	this.device=function(){
-		var deviceInfo=navigator.userAgent;
-		if(deviceInfo.indexOf("Windows")==-1){
-			this.isPhone=true;
-			this.canvas.width=window.innerWidth;
-			this.canvas.height=window.innerHeight;
-			this.width=window.innerWidth;
-			this.height=window.innerHeight;
-			this.stepX=this.width/this.step;
-			this.stepY=this.height/this.step;
-			console.log(this.width+":"+this.height);
-			console.log(this.canvas.height);
-		}
 	}
 	/*
-	 3.蛇的移动
-	 3.1判断设备是pc还是移动
-	 3.2生成键盘事件 和 触屏事件
-	 * */
-	this.keyHandler=function(){//键盘事件
-		var _this = this;
-		document.onkeydown = function(ev){
-			var ev = ev||window.event;
-//			console.log(_this.snakeBodyList);
-			switch(ev.keyCode){
-				case 37://向左
-					_this.snakeBodyList[0].img = westImg;
-					_this.snakeBodyList[0].direct = 'west';
-				break;
-				case 38://向上
-					_this.snakeBodyList[0].img = northImg;
-					_this.snakeBodyList[0].direct = 'north';
-				break;
-				case 39://向右
-					_this.snakeBodyList[0].img = eastImg;
-					_this.snakeBodyList[0].direct = 'east';
-				break;
-				case 40://向下
-					_this.snakeBodyList[0].img = southImg;
-					_this.snakeBodyList[0].direct = 'south';
-				break;
-			}
-		}
-	};
-	this.touchHandler=function(event){//触屏事件
-	var _this=this;
-	document.addEventListener("touchstart",function(ev){
-//		ev.preventDefault();
-//		console.log(ev);
-var touchX=ev.changedTouches[0].clientX;
-var touchY=ev.changedTouches[0].clientY;
-var head=_this.snakeBodyList[0];
-var headX=head.x*_this.step;
-var headY=head.y*_this.step;
-if(head.direct=="north"||head.direct=="south"){
-	if(touchX<headX){
-		head.direct="west";
-		head.img=westImg;
-	}else{
-		head.direct="east";
-		head.img=eastImg;
-	}
-}else if(head.direct == "west" || head.direct == "east"){
-	if(touchY < headY){
-					head.direct = "north";
-					head.img = northImg;
-				}else{
-					head.direct = "south";
-					head.img = southImg;
-				}
-			
-}
-
-	})
-	};
-this.move=function(){
-//事件处理是异步的，所以，无法传递this对象
-if(!this.isPhone){
-	this.keyHandler();
-}else{
-	this.touchHandler();
-}
-    var _this = this;  
-	//运用定时器，每隔0.2秒移动蛇（舍得坐标变化，然后重绘）
-	this.timer=setInterval(function(){
-		//首先：解决蛇身跟随的问题；
-		for(var i=_this.snakeBodyList.length-1;i>0;i--){
-			_this.snakeBodyList[i].x=_this.snakeBodyList[i-1].x;
-			_this.snakeBodyList[i].y=_this.snakeBodyList[i-1].y;
-		}
-		//其次：根据方向及坐标，处理舌头的移动新坐标
-		var sHead=_this.snakeBodyList[0];
-		switch(sHead.direct){
-			case 'north':
-			
-			sHead.y-=1;
-			break;
-			case 'south':
-			
-			sHead.y+=1;
-			break;
-			case 'west':
-			
-			sHead.x-=1;
-			break;
-			case 'east':
-			
-			sHead.x+=1;
-			break;
-		}
-		_this.start();
-	//3.1.1 判断，蛇移动后新位置是否已经触边界，或触自身  true--dead
-	_this.dead();//判断蛇生死，isDead
-		if(_this.isDead){
-			//alert你的最终分数
-			alert("Your score is:"+_this.score);
-			//重新开始游戏restart（）方法
-			clearInterval(_this.timer);//如果不清除定时器，则速度会不断加快
-			_this.isDead = false;//改变isDead状态，否则，每次直接死掉
-			_this.snakeBodyList = [];//清除蛇身，便于重新开始游戏，重绘初始界面
-			_this.open();//游戏重新开始
-		}else{
-			//3.1.2 false：蛇活着，判断蛇头是否与食物的坐标点一致，如果一致，清空食物数组（多个食物时，可以使用标识位）
-			_this.eat();//判断食物是否被吃，isEaten
-			if(_this.isEaten){
-			
-				_this.isEaten = false;
-				//清空食物数组
-				_this.foodList = [];
-				//加分
-				_this.score += 10;
-				//蛇身长一节
-				var lastNodeIndex = _this.snakeBodyList.length;
-				_this.snakeBodyList[lastNodeIndex] = {
-					x:-2,
-					y:-2,
-					img:bodyImg,
-					direct:_this.snakeBodyList[lastNodeIndex-1].direct
-				};
-			}
-			//3.1.3 否则重绘
-			_this.start();//重绘游戏画面（背景+蛇+食物）
-		}
-	},200);
-		
-	}
-	/*
-	 2.2画蛇：算法【{x:横坐标，y:纵坐标，img:图片，direct:运动方向}】
-	 * */
-	this.drawSnake=function(){
-		//2.2.1循环生成snakeBodyList数组中的对象集合（默认，蛇居中）
+	 * 2.2画蛇：算法[{x:横坐标，y：纵坐标，img：图片，direct：运动方向},.......]
+	 */
+	this.drawSnake = function() {
+		//2.2.1循环生成snakeBodyList数组中的对象集合 （默认，蛇居于中间，蛇头向西）
 		if(this.snakeBodyList.length<5){
-		for(let i=0;i<5;i++){
-			//【{x:横坐标，y:纵坐标，img:图片，direct:运动方向}】
+		for(var i = 0; i < 5; i++) {
+			//{x:横坐标，y：纵坐标，img：图片，direct：运动方向}蛇的节点设计
 			this.snakeBodyList.push({
-				x:Math.floor(this.stepX)/2+i-2,//注意：x不是px像素坐标点而是x轴步数
-				y:Math.floor(this.stepY)/2,//注意：y轴的步数；
-				img:bodyImg,
-				direct:"west",
-			})
+				x: Math.floor(this.stepX / 2) + i - 2, //注意：x不是px像素坐标点，而是x轴步数
+				y: Math.floor(this.stepY / 2), //注意：这是y轴步数
+				img: bodyImg,
+				direct: "west"
+			});
+		}
+		//2.2.2替换snakeBodyList数组第一个元素的img，替换成westImg蛇头图片
+		this.snakeBodyList[0].img = westImg;
+		}
+		//2.2.3遍历snakeBodyList数组，并画出蛇的初始状态
+		for(var i = 0; i < this.snakeBodyList.length; i++) {
+			var snode = this.snakeBodyList[i];
+			this.ctx.drawImage(snode.img, snode.x * this.step,
+				snode.y * this.step, this.step, this.step);
 		}
 		
-		this.snakeBodyList[0].img=westImg;
-		//2.2.2替换snakeBodyList  数组第一个元素的img替换成westimg图片
-		}
-		
-		
-//		2.2.3遍历snakeBodyList，并画出蛇的初始状态
-     for(let i=0;i<this.snakeBodyList.length;i++){
-			var Node=this.snakeBodyList[i];
-			this.ctx.drawImage(Node.img,Node.x*this.step,Node.y*this.step,this.step,this.step);
-		}
+
 	}
 	/*
-	 画食物
-	 * */
-	this.drawFood=function(){
-		/*2.3.1当食物已经存在的时候，画面刷新时食物在原有位置重绘
-		 */
+	 * 2.3画食物
+	 */
+	this.drawFood = function() {
+
+		//2.3.1当食物已经存在的时候，画面刷新时，食物在原有位置重绘
 		if(this.foodList.length > 0) {
 			var fnode = this.foodList[0];
 			this.ctx.drawImage(fnode.img, fnode.x * this.step, fnode.y * this.step, this.step, this.step);
@@ -258,16 +142,162 @@ if(!this.isPhone){
 				y: foodY,
 				img: foodImg
 			}); //新生成一个食物
+			var asd=parseInt(Math.random()*3+1);
+			foodImg.src="image/food"+asd+".png";
+			foodImg.data = asd;
 			var fnode = this.foodList[0];
 			this.ctx.drawImage(fnode.img, fnode.x * this.step, fnode.y * this.step, this.step, this.step);
 		}
-		
-		
+
 	}
 	/*
-	 4.蛇死(碰到边界 或碰到自身)
-	 * */
-	this.dead=function(){
+	 * 3-蛇动（事件改变蛇移动方向，判断蛇是否死掉，然后判断蛇是否吃了食物，之后蛇移动）
+	 * 3.1 判断设备如果是pc，响应键盘事件，否则，响应触屏事件
+	 * 3.2 生成键盘事件处理器this.keyHandler()，和触屏事件处理器this.touchHandler()
+	 */
+	this.keyHandler = function(){//键盘事件处理器
+			//事件处理是异步的，所以，无法传递this对象
+		var _this = this;
+		document.onkeydown = function(ev){
+			var ev = ev||window.event;
+//			console.log(ev.key+":"+ev.keyCode);
+//			console.log(_this.snakeBodyList);
+			switch(ev.keyCode){
+				case 37://向左
+					_this.snakeBodyList[0].img = westImg;
+					_this.snakeBodyList[0].direct = 'west';
+				break;
+				case 38://向上
+					_this.snakeBodyList[0].img = northImg;
+					_this.snakeBodyList[0].direct = 'north';
+				break;
+				case 39://向右
+					_this.snakeBodyList[0].img = eastImg;
+					_this.snakeBodyList[0].direct = 'east';
+				break;
+				case 40://向下
+					_this.snakeBodyList[0].img = southImg;
+					_this.snakeBodyList[0].direct = 'south';
+				break;
+			}
+		}
+	}
+	
+	this.touchHandler = function(){//触屏事件处理器
+		var _this = this;
+		document.addEventListener("touchstart",function(ev){
+			var ev = ev||window.event;
+//			console.log(ev);
+			var touchX = ev.changedTouches[0].clientX;
+			var touchY = ev.changedTouches[0].clientY;
+			console.log(touchX+":"+touchY);
+			var head = _this.snakeBodyList[0];
+			var headX = head.x*_this.step;//注意蛇头x坐标值与px的转换  乘以_this.step
+			var headY = head.y*_this.step;//注意蛇头x坐标值与px的转换  乘以_this.step
+			if(head.direct == "north" || head.direct == "south"){
+				if(touchX < headX){
+					head.direct = "west";
+					head.img = westImg;
+				}else{
+					head.direct = "east";
+					head.img = eastImg;
+				}
+			}else if(head.direct == "west" || head.direct == "east"){
+				if(touchY < headY){
+					head.direct = "north";
+					head.img = northImg;
+				}else{
+					head.direct = "south";
+					head.img = southImg;
+				}
+			}
+			
+		})
+		
+	}
+	//添加change方法
+
+	
+	
+	this.move = function() {
+		var sss=$("#rang").val();
+		if(!this.isPhone){
+			this.keyHandler();
+		}else{
+			this.touchHandler();
+		}	
+		//3.1运用定时器，每隔0.2秒移动蛇（蛇的坐标变化，然后重绘）
+		var _this = this;
+		this.timer = setInterval(function(){
+			//首先：解决蛇身跟随的问题
+			for(var i = _this.snakeBodyList.length-1;i>0;i--){
+				_this.snakeBodyList[i].x = _this.snakeBodyList[i-1].x;
+				_this.snakeBodyList[i].y = _this.snakeBodyList[i-1].y;
+			}
+			//其次，根据方向及坐标，处理蛇头的移动新坐标
+			var shead = _this.snakeBodyList[0];
+			switch(shead.direct){
+				case 'north':
+					shead.y--;
+				break;
+				case 'south':
+					shead.y++;
+				break;
+				case 'west':
+					shead.x--;
+				break;
+				case 'east':
+					shead.x++;
+				break;
+			}
+		//3.1.1 判断，蛇移动后新位置是否已经触边界，或触自身  true--dead
+		_this.dead();//判断蛇生死，isDead
+		if(_this.isDead){
+			//alert你的最终分数
+			alert("Your score is:"+_this.score);
+			//重新开始游戏restart（）方法
+			clearInterval(_this.timer);//如果不清除定时器，则速度会不断加快
+			_this.isDead = false;//改变isDead状态，否则，每次直接死掉
+			_this.snakeBodyList = [];//清除蛇身，便于重新开始游戏，重绘初始界面
+			_this.start();//游戏重新开始
+//			location=location;
+		}else{
+			//3.1.2 false：蛇活着，判断蛇头是否与食物的坐标点一致，如果一致，清空食物数组（多个食物时，可以使用标识位）
+			_this.eat();//判断食物是否被吃，isEaten
+			if(_this.isEaten){
+//				console.log("*********eaten***********");
+				_this.isEaten = false;
+				//清空食物数组
+				_this.foodList = [];
+				//加分
+				
+				switch(foodImg.data){
+					case 1 :_this.score += 10;
+					
+					break;
+					case 2 :_this.score += 20;
+					break;	
+					case 3 :_this.score += 30;
+					break;	
+				}
+				//蛇身长一节
+				var lastNodeIndex = _this.snakeBodyList.length;
+				_this.snakeBodyList[lastNodeIndex] = {
+					x:-2,
+					y:-2,
+					img:bodyImg,
+					direct:_this.snakeBodyList[lastNodeIndex-1].direct
+				};
+			}
+			//3.1.3 否则重绘
+			_this.paint();//重绘游戏画面（背景+蛇+食物）
+		}
+		},sss);
+	}
+	/*
+	 * 4-蛇死（碰到边界或碰到自身--dead 弹出得分界面）
+	 */
+	this.dead = function() {
 		const LEFT_END = 0;//左边界
 		const RIGHT_END = this.stepX;//右边界
 		const NORTH_END = 0;//上边界
@@ -287,9 +317,9 @@ if(!this.isPhone){
 		}
 	}
 	/*
-	 5.分数
-	 * */
-	this.eat=function(){
+	 * 5-蛇吃食物（蛇头坐标与食物坐标一致）
+	 */
+	this.eat = function(){
 		const HEAD_X = this.snakeBodyList[0].x;//蛇头横坐标x
 		const HEAD_Y = this.snakeBodyList[0].y;//蛇头纵坐标y
 		const FOOD_X = this.foodList[0].x;//食物横坐标x
