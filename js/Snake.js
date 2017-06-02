25f03123870f11cee6168c1cc9693917bf24795a
ming <ming@qq.com>
2017-06-02 13:32:26 +0800

    version2.2.3 replace

--- js/Snake.js
+++ js/Snake.js
@@ -277,11 +277,27 @@ function Snake() {
 		if(_this.isDead){
 			//alert你的最终分数
 			alert("Your score is:"+_this.score);
+			//localstorage添加分数
+//			var numm=0;
+//	        if(!localStorage.numm){
+//		      localStorage.setItem("numm",parseInt(numm));
+//	        }
+//	        mast=_this.score;
+//	        localStorage.setItem("numm",mast);
+////	        location=location;
+//	        getScroe();
+//		function getScroe(){
+//		
+//		var keys1=localStorage.key(0);
+//		var val1=localStorage.getItem(keys1);
+//		   console.log(val1);
+//		   }
+	       
 			//重新开始游戏restart（）方法
 			clearInterval(_this.timer);//如果不清除定时器，则速度会不断加快
 			_this.isDead = false;//改变isDead状态，否则，每次直接死掉
 			_this.snakeBodyList = [];//清除蛇身，便于重新开始游戏，重绘初始界面
-			_this.startmm();//游戏重新开始
+			_this.start();//游戏重新开始
 //			location=location;
 		}else{
 			//3.1.2 false：蛇活着，判断蛇头是否与食物的坐标点一致，如果一致，清空食物数组（多个食物时，可以使用标识位）
